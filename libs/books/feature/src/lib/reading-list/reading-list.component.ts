fa1ada8d48f27b907624452cd52197ff5a353a56
Sudharshan Talari <stalari.dev@gmailcom>
2020-09-15 04:29:08 -0400

    Task-3: Undo add and remove from reading list.

--- libs/books/feature/src/lib/reading-list/reading-list.component.ts
+++ libs/books/feature/src/lib/reading-list/reading-list.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Store } from '@ngrx/store';
-import { getReadingList, removeFromReadingList } from '@tmo/books/data-access';
+import { getReadingList, removeFromReadingList, undoRemoveFromReadingList } from '@tmo/books/data-access';
+import { ReadingListItem } from '@tmo/shared/models';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'tmo-reading-list',
@@ -10,9 +13,16 @@ import { getReadingList, removeFromReadingList } from '@tmo/books/data-access';
 export class ReadingListComponent {
   public readingList$ = this.store.select(getReadingList);
 
-  constructor(private readonly store: Store) {}
+  constructor(
+    private readonly store: Store,
+    private readonly snackBar: MatSnackBar
+  ) {}
 
-  public removeFromReadingList(item): void {
+  public removeFromReadingList(item: ReadingListItem): void {
     this.store.dispatch(removeFromReadingList({ item }));
+    // Note: No input was given on how long the snackbar should display. 
+    // So, to accomodate screenreader users (Accessibility), duration param was not added for the snackbar.
+    const snackBarRef = this.snackBar.open(`${item.title} removed from your reading list.`, 'Undo');
+    snackBarRef.onAction().pipe(take(1)).subscribe(() => this.store.dispatch(undoRemoveFromReadingList({ item })));
   }
 }
